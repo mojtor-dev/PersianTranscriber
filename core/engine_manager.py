0a88d079b9899c877b24d862d1f3253a092307a3
mojtor-dev <moshtaba.tor@gmail.com>
2026-07-10 09:05:58 +0330

    Add whisper cpp engine support

--- core/engine_manager.py
+++ core/engine_manager.py
@@ -4,6 +4,7 @@ Version: 0.1.0
 """
 
 from core.engines.whisper import WhisperEngine
+from core.engines.whisper_cpp import WhisperCppEngine
 from core.config_loader import ConfigLoader
 
 
@@ -14,11 +15,19 @@ class EngineManager:
         self.config = ConfigLoader()
 
         self.engines = {
-    "whisper": WhisperEngine(
-        model=self.config.get_model(),
-        language=self.config.get_language()
-    )
-}
+
+            "whisper": WhisperEngine(
+                model=self.config.get_model(),
+                language=self.config.get_language()
+            ),
+
+            "whisper_cpp": WhisperCppEngine(
+                model=self.config.get_model(),
+                language=self.config.get_language()
+            )
+
+        }
+
     def get_engine(self):
 
         name = self.config.get_engine_name()
