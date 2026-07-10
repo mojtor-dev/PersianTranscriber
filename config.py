a4b34702b6653ff72042b9d8d9d6b73d322dd543
mojtor-dev <moshtaba.tor@gmail.com>
2026-07-10 06:02:01 +0330

    Add application configuration module

---
+++ config.py
@@ -0,0 +1,17 @@
+"""
+PersianTranscriber Configuration
+Version: 0.1.0
+"""
+
+APP_NAME = "PersianTranscriber"
+
+VERSION = "0.1.0"
+
+LANGUAGE = "fa"
+
+DEFAULT_OUTPUT_FOLDER = "output"
+
+DEFAULT_LOG_FOLDER = "logs"
+
+DEFAULT_TEMP_FOLDER = "temp"
+
