5b71d240da56f4df1575b3adba5597c4f46fbaee
mojtor-dev <moshtaba.tor@gmail.com>
2026-07-10 06:34:58 +0330

    Add audio loader module

---
+++ core/audio_loader.py
@@ -0,0 +1,33 @@
+"""
+PersianTranscriber Audio Loader
+Version: 0.1.0
+"""
+
+import os
+
+
+class AudioLoader:
+
+    SUPPORTED_FORMATS = [
+        ".mp3",
+        ".wav",
+        ".m4a",
+        ".flac"
+    ]
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def exists(self):
+        return os.path.exists(self.file_path)
+
+    def get_extension(self):
+        return os.path.splitext(self.file_path)[1].lower()
+
+    def is_supported(self):
+        return self.get_extension() in self.SUPPORTED_FORMATS
+
+    def get_size(self):
+        if self.exists():
+            return os.path.getsize(self.file_path)
+        return 0
